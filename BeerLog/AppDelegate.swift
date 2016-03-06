f932a3f049ecbbd00cf490799ffc1871d0b46f4e
Kengo Yoshii <dr.sunoo@gmail.com>
2016-03-06 09:29:41 +0900

    changed the tab bar selected color to orange from default blue

--- BeerLog/AppDelegate.swift
+++ BeerLog/AppDelegate.swift
@@ -15,6 +15,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+
+        UITabBar.appearance().tintColor = BeerLogDifinition.TITLEBAR_COLOR
+
         return true
     }
 
